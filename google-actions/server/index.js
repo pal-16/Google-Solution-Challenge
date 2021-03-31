e9617a3d23b474d9df89eac2ddc7c4d457658875
pal-16 <mantrypalak@gmail.com>
2021-04-01 00:53:58 +0530

    added server and client integration code with location details

--- google-actions/server/index.js
+++ google-actions/server/index.js
@@ -3,13 +3,53 @@ const dfff = require('dialogflow-fulfillment');
 const request = require('request');
 const axios = require('axios')
 const app = express();
-var convert = require('xml-js');
 app.set('views', __dirname + '/views');
 app.engine('html', require('ejs').renderFile);
-
+const cors = require("cors");
+var convert = require('xml-js');
+app.use(cors());
 app.get('/', (_, res)=>{
  res.send("connected");
 });
+
+
+
+app.post('/map/:crop/:location', (req, res) => {
+
+    const crop = req.params.crop;
+    const location = req.params.location;
+    //const location = "borivali";
+    const searchstr = `${crop}+store+in+${location}`
+    const url = "https://maps.googleapis.com/maps/api/place/textsearch/xml?query=" + searchstr + "&key=AIzaSyBG0jpJ-VBkYhhKsvjBZ2_dWDLbFkNSQ7M"
+    console.log(url);
+    axios.get(url)
+        .then(function (response) {
+            console.log(response);
+        var result = convert.xml2json(response['data'], {compact: true, spaces: 4});
+        var jsonRes=JSON.parse(result);
+        console.log(jsonRes["PlaceSearchResponse"]["result"]);
+        var len_response = jsonRes["PlaceSearchResponse"]["result"].length;
+        console.log(len_response)
+        var finalCoordinates=[];
+        var i;
+        if (len_response>10)
+        len_response=10;
+        for(i=0;i<len_response;i++){
+            var lat;
+            var lng;
+            lat= jsonRes["PlaceSearchResponse"]["result"][i]["geometry"]["location"]["lat"]["_text"];
+            lng= jsonRes["PlaceSearchResponse"]["result"][i]["geometry"]["location"]["lng"]["_text"];
+         
+            finalCoordinates.push({"lat":lat, "lng":lng});
+        }
+        console.log(finalCoordinates);     
+        res.status(200).json(finalCoordinates);
+        })
+       .catch(error => {
+           res.status(500).json(error);
+        })
+});
+
 
 app.post('/', express.json(), (req,res)=>{
    
@@ -20,51 +60,47 @@ app.post('/', express.json(), (req,res)=>{
 
     function askParameters(agent){
 
-            console.log(agent.context);
-            var allValues = agent.context.get("awaiting_parameters").parameters
-
-            //send request by getting parameters from dialgoflow  and get the answer
-            axios
-            .post('https://2ce38ca33df3.ngrok.io/crop_predictor', {
-                
-                    "N":85, 
-                    "P":58,
-                    "K":41,
-                    "temperature":21.77046169,
-                    "humidity": 80.31964408, 
-                    "ph":7.038096361,
-                    "rainfall":226.6555374
-                
-            })
-            .then(res => {
-                console.log(`statusCode: ${res.statusCode}`)
-                console.log(res)
-            })
-            .catch(error => {
-                console.error(error)
-            })
-
-        //send crop name and get location as the answer
-                var result;
-                axios.get("https://maps.googleapis.com/maps/api/place/textsearch/xml?query=bajra+shop+in+borivali&key=AIzaSyBG0jpJ-VBkYhhKsvjBZ2_dWDLbFkNSQ7M")
-                .then(function(response) {
-                    result = convert.xml2json(response['data'], {compact: true, spaces: 4});
-                console.log(result)
-                res.render('location.ejs',{content:result}) 
-                })
-                .catch(error => {
-                    console.error(error)
-                })    
-                                                    
-            console.log(allValues);
-            agent.add(`Details saved.`);      
+        var allValues = agent.context.get("awaiting_parameters").parameters
+        console.log(allValues);
+        //send request by getting parameters from dialogflow  and get the answer
+        axios
+        .post('http://127.0.0.1:5000/crop_predictor', {
+            
+              /*  "N":allValues['nitrogen'], 
+                "P":allValues['phosphorous'],
+                "K":allValues['potassium'],
+                "temperature":allValues['temperature'],
+                "humidity": allValues['humidity'], 
+                "ph":allValues['ph'],
+                "rainfall": allValues['rainfall']*/
+    
+               "N":85, 
+                "P":58,
+                "K":41,
+                "temperature":21.77046169,
+                "humidity": 80.31964408, 
+                "ph":7.038096361,
+                "rainfall":226.6555374
+            
+        })
+        .then(res => {
+            console.log("============");
+            console.log(res["data"]);
+            //var cropName = res["data"];
+            var cropName="rice";
+            var location = allValues['location']; 
+            agent.add("We have found that + "+cropName+"crop is good. \n  You can find the nearest crop centers near you on this link \n https://3c4ce90e09da.ngrok.io/locate?crop="+cropName+"&location="+location);
+        
+        })
+        .catch(error => {
+            console.error(error)
+        })
+      
     }
-
     var intentMap = new Map();
     intentMap.set('askParameters', askParameters)
- 
     agent.handleRequest(intentMap);
-
+   
 });
 
 app.listen(8000, ()=>console.log("Server is live at port 8000"));
